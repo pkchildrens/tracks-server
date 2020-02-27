493775ab124f48cd8196a53032045c7b2354f897
Pradeep <pradeep.kumar@childrens.com>
2020-02-26 20:39:45 -0600

    init

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = mongoose.model('User')
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = new User({ email, password })
+    await user.save()
+
+    const token = jwt.sign({ userId: user._id }, 'mysecretkey')
+    res.send({ token })
+  } catch (e) {
+    return res.status(422).send(e.message)
+  }
+
+})
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provider email and password' })
+  }
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    return res.status(404).send({ error: 'Invalid password or email' })
+  }
+
+  try {
+    await user.comparePassword(password)
+    const token = jwt.sign({ userId: user._id }, 'mysecretkey')
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email' })
+  }
+})
+
+module.exports = router
